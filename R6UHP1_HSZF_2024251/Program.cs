1397349650521ca8f83d313d8d1b88c8372a36e6
Polgár Kristóf <polgarkristof10@gmail.com>
2024-11-21 12:25:50 +0100

    initial commit

---
+++ R6UHP1_HSZF_2024251/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace R6UHP1_HSZF_2024251
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alma
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
