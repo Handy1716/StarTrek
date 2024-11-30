3de2156df06019af80728eb3780ee1a2ded53fd6
Polgár Kristóf <polgarkristof10@gmail.com>
2024-11-30 17:23:03 +0100

    jolmukodoadatbazisbeolvasas

--- R6UHP1_HSZF_2024251.Application/Services/DataImportService.cs
+++ R6UHP1_HSZF_2024251.Application/Services/DataImportService.cs
@@ -13,35 +13,41 @@ namespace R6UHP1_HSZF_2024251.Persistence.MsSql.Infrastructure.Services
         public void ReadIn()
         {
             XDocument xdoc = XDocument.Load("StarTrekData.xml");
-            var spaceships = xdoc.Descendants("Spaceship").Select(x => {
+            var spaceships = xdoc.Descendants("Spaceship").Select(x =>
+            {
                 var newSpaceShip = new SpaceShip
                 {
                     Name = (string)x.Element("Name"),
                     Type = (SpaceShip.SpaceShipType)Enum.Parse(typeof(SpaceShip.SpaceShipType), (string)x.Element("Type")),
                     CrewCount = (int)x.Element("CrewCount"),
                     Status = (SpaceShip.SpaceShipStatus)Enum.Parse(typeof(SpaceShip.SpaceShipStatus), (string)x.Element("Status")),
-                    PlanetId = (int)x.Element("PlanetId"),
+                    PlanetId = x.Element("PlanetId") != null ? (int?)x.Element("PlanetId") : null,
                 };
                 return newSpaceShip;
             }).ToList();
 
-            var planets = xdoc.Descendants("Planet").Select(x => {
+            var planets = xdoc.Descendants("Planet").Select(x =>
+            {
                 var newPlanet = new Planet
                 {
                     Name = (string)x.Element("Name"),
                     Type = (Planet.PlanetType)Enum.Parse(typeof(Planet.PlanetType), (string)x.Element("Type")),
-                    ExplorationShipId = (int)x.Element("ExplorationShipId"),
+                    ExplorationShipId = x.Element("ExplorationShipId") != null ? (int?)x.Element("ExplorationShipId") : null
+                    
                 };
+                
+
                 return newPlanet;
             }).ToList();
 
-            var crewmembers = xdoc.Descendants("CrewMember").Select(x => {
+            var crewmembers = xdoc.Descendants("CrewMember").Select(x =>
+            {
                 var newCrewMember = new CrewMember
                 {
                     Name = (string)x.Element("Name"),
                     Rank = (CrewMember.CrewMemberRank)Enum.Parse(typeof(CrewMember.CrewMemberRank), (string)x.Element("Rank")),
                     SpaceShipId = (int)x.Element("SpaceshipId"),
-                    MissionCount =  (int?)x.Element("MissionCount"),
+                    MissionCount = x.Element("MissionCount") != null ? (int?)x.Element("MissionCount") : null,
                 };
                 return newCrewMember;
             }).ToList();
@@ -52,7 +58,7 @@ namespace R6UHP1_HSZF_2024251.Persistence.MsSql.Infrastructure.Services
                 {
                     TargetPlanetId = (int)x.Element("TargetPlanetId"),
                     StartDate = DateTime.Parse((string)x.Element("StartDate")),
-                    EndDate = DateTime.Parse((string)x.Element("EndDate")),
+                    EndDate = x.Element("EndDate") != null ? (DateTime?)DateTime.Parse((string)x.Element("EndDate")) : null,
                     Status = (Mission.MissionStatus)Enum.Parse(typeof(Mission.MissionStatus), (string)x.Element("Status")),
                 };
                 return newMission;
--- R6UHP1_HSZF_2024251.Persistence.MsSql/obj/Debug/net8.0/R6UHP1_HSZF_2024251.Persistence.MsSql.AssemblyInfo.cs
+++ R6UHP1_HSZF_2024251.Persistence.MsSql/obj/Debug/net8.0/R6UHP1_HSZF_2024251.Persistence.MsSql.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("R6UHP1_HSZF_2024251.Persistence.MsSql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e0eade2fd91aae18a145b7c696d26bd6399f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d782c0c5a455224a678c2553fbecd4a0d9595a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("R6UHP1_HSZF_2024251.Persistence.MsSql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("R6UHP1_HSZF_2024251.Persistence.MsSql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
